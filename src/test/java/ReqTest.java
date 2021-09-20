63aae455d55ca308940804b233b76b37fc585ed5
ChristianJaimes <christiandja@gmail.com>
2021-09-20 09:57:26 -0500

    Se añade asercion "notNull" y se crea test para metodo get Sigle user

--- src/test/java/ReqTest.java
+++ src/test/java/ReqTest.java
@@ -1,11 +1,13 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ReqTest {
     @Test
     public void loginTest(){
-        String response = RestAssured
+        RestAssured
                 .given()
                 .log().all()
                 .contentType(ContentType.JSON)
@@ -16,7 +18,20 @@ public class ReqTest {
                 .post("https://reqres.in/api/login")
                 .then()
                 .log().all()
-                .statusCode(200).
+                .statusCode(200)
+                .body("token", notNullValue());
 
+    }
+    @Test
+    public void getSingleUserTest(){
+        RestAssured
+                .given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("data.id",equalTo(2));
     }
 }
