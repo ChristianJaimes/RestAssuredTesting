afc26c93d21794c0c93af96c6e76cd06273670d8
ChristianJaimes <christiandja@gmail.com>
2021-09-20 11:50:31 -0500

    Se añade @Before para evitar reptir codigo, se importa RestAsured como Estatico y se cambia el codigo 200 por HttpStatus.SC_OK

--- src/test/java/ReqTest.java
+++ src/test/java/ReqTest.java
@@ -1,37 +1,46 @@
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.Test;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
 
 public class ReqTest {
+
+    @Before
+    public void setUp(){
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+        RestAssured.filters(new RequestLoggingFilter(),new ResponseLoggingFilter());
+    }
+
     @Test
     public void loginTest(){
-        RestAssured
-                .given()
-                .log().all()
+                given()
                 .contentType(ContentType.JSON)
                 .body("{\n" +
                         "    \"email\": \"eve.holt@reqres.in\",\n" +
                         "    \"password\": \"cityslicka\"\n" +
                         "}")
-                .post("https://reqres.in/api/login")
+                .post("login")
                 .then()
-                .log().all()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)//codigo 200
                 .body("token", notNullValue());
 
     }
     @Test
     public void getSingleUserTest(){
-        RestAssured
-                .given()
-                .log().all()
+        given()
                 .contentType(ContentType.JSON)
-                .get("https://reqres.in/api/users/2")
+                .get("sers/2")
                 .then()
                 .log().all()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)//
                 .body("data.id",equalTo(2));
     }
 }
