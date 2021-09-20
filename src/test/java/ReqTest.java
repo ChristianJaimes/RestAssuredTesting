aeec9d925cd0491f9830820cddd6ff37cfb6756e
ChristianJaimes <christiandja@gmail.com>
2021-09-20 09:40:08 -0500

    Se Crea clase ReqTest

---
+++ src/test/java/ReqTest.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+public class ReqTest {
+    @Test
+    public void loginTest(){
+        String response = RestAssured
+                .given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"cityslicka\"\n" +
+                        "}")
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().all()
+                .extract()
+                .asString();
+        System.out.println(response);
+    }
+}
